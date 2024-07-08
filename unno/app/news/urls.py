2af9baac2b6e441fc337d327b13af5330d6eb65b
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-07-09 00:51:28 +0800

    feat: news page urls

--- unno/app/news/urls.py
+++ unno/app/news/urls.py
@@ -1,9 +1,18 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import NewsViewSet
+from . import views
 
+
+# api
 router = SimpleRouter()
 
-router.register("", NewsViewSet, basename="news")
+router.register("", views.NewsViewSet, basename="news")
+
+# html
+page_urlpatterns = [
+    path("news/", views.NewsListView.as_view(), name="news-list"),
+    path("news/<pk>", views.NewsDetailView.as_view(), name="news-detail"),
+]
 
 urlpatterns = router.urls
--- unno/app/news/views.py
+++ unno/app/news/views.py
@@ -1,9 +1,18 @@
+from django.views.generic import TemplateView
 from rest_framework.pagination import CursorPagination
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
 
 from .models import News
 from .serializers import NewsListSerializer, NewsSerializer
+
+
+class NewsListView(TemplateView):
+    template_name = "news-list.html"
+
+
+class NewsDetailView(TemplateView):
+    template_name = "news-detail.html"
 
 
 # Create your views here.
