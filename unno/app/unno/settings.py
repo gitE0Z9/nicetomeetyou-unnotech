6f922edb57a5bd8fb1a577508757248be6fb76fe
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-07-09 00:52:13 +0800

    feat: staticfiles dir

--- unno/app/unno/settings.py
+++ unno/app/unno/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
