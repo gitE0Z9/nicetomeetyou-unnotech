d2188623fee95f1a76f7c9a8e767bf82451d7cde
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-07-09 00:52:00 +0800

    feat: register all page urls to project urls

--- unno/app/unno/urls.py
+++ unno/app/unno/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("unno.version1.urls")),
+    path("", include("unno.pages.urls")),
     path("admin/", admin.site.urls),
 ]
