9874286a9736dedea6d01fe16c4c24d38316b887
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-07-10 00:41:47 +0800

    refactor: crawl task name as function name

--- unno/app/scraper/tasks.py
+++ unno/app/scraper/tasks.py
@@ -1,6 +1,6 @@
 from .spiders.udn_nba import UdnNbaSpider
 
 
-def crawl_udn_nba_task(max_page: int = 10):
+def crawl_udn_nba(max_page: int = 10):
     spider = UdnNbaSpider(channel_id=2000, max_page=max_page)
     spider.run()
