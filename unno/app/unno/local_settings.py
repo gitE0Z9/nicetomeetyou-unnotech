a7002832b2f26e6c310aa37bf47082f47419b5ba
gitE0Z9 <acrocanthosaurus627@gmail.com>
2024-07-10 00:42:12 +0800

    refactor: secret key got default value

--- unno/app/unno/local_settings.py
+++ unno/app/unno/local_settings.py
@@ -2,7 +2,10 @@ import os
 from .settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DEBUG_SECRET_KEY"]
+SECRET_KEY = os.environ.get(
+    "DEBUG_SECRET_KEY",
+    "django-insecure-@po6py(s9n$e_8ez!-#71()+xqzn=+$j10@(7r%ew*e%9-!it#",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
