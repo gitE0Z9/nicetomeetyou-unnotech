a08b9a7a6c5a180119577dd309d70a8ff9c0c2cc
mike bai <g56685@yahoo.com.tw>
2024-07-10 14:50:20 +0800

    refactor: return created count when finish crawling

--- unno/app/scraper/tasks.py
+++ unno/app/scraper/tasks.py
@@ -3,4 +3,6 @@ from .spiders.udn_nba import UdnNbaSpider
 
 def crawl_udn_nba(max_page: int = 10):
     spider = UdnNbaSpider(channel_id=2000, max_page=max_page)
-    spider.run()
+    created_count = spider.run()
+
+    return created_count
