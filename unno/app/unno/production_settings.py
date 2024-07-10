4b05f99351b7054a7a3e04d0a07f650fcd2340fc
mike bai <g56685@yahoo.com.tw>
2024-07-10 12:54:27 +0800

    feat: during production, use postgres as default db

--- unno/app/unno/production_settings.py
+++ unno/app/unno/production_settings.py
@@ -9,3 +9,14 @@ ALLOWED_HOSTS = ["weitravel.herokuapp.com"]
 DEBUG = False
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ["DB_HOST"],
+        "PORT": os.environ["DB_PORT"],
+    }
+}
