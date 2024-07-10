46f75fbdd9833ef42cfaf48104eb0667335b1fa9
mike bai <g56685@yahoo.com.tw>
2024-07-10 14:50:41 +0800

    feat: finish_crawl_udn_nba hooks

---
+++ unno/app/scraper/hooks.py
@@ -0,0 +1,18 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+
+def finish_crawl_udn_nba(task):
+    def send_ws_msg(channel_name: str, type: str, data: dict):
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            channel_name,
+            {
+                "type": type,
+                **data,
+            },
+        )
+
+    if task.result:
+        send_ws_msg({"updated": 1})
