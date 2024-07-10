852f360379464c505113da6e71afb5c38350a7fd
mike bai <g56685@yahoo.com.tw>
2024-07-10 15:41:27 +0800

    fix: asgi staticfiles urls

--- unno/app/unno/urls.py
+++ unno/app/unno/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("unno.version1.urls")),
     path("", include("unno.pages.urls")),
     path("admin/", admin.site.urls),
+    *staticfiles_urlpatterns(),
 ]
